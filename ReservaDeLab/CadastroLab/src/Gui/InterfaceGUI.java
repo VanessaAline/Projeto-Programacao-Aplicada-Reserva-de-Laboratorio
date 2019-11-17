8318c19cd283efa754872cbe53e6ee020187cb40
andreyroberto <48372956+andreyroberto@users.noreply.github.com>
2019-11-16 21:28:39 -0300

    Update InterfaceGUI.java

--- ReservaDeLab/CadastroLab/src/Gui/InterfaceGUI.java
+++ ReservaDeLab/CadastroLab/src/Gui/InterfaceGUI.java
@@ -10,6 +10,9 @@ import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,9 +26,10 @@ import javax.swing.border.MatteBorder;
 public class InterfaceGUI {
 	//variaveis
 	JFrame tela = new JFrame("Reserva de Laboratórios");
-	JPanel panel, panelCadastro;
+	JPanel panel, panelCadastro, panelAcesso;
 	ImageIcon icon = new ImageIcon(getClass().getResource("untitled.png"));
-	JLabel Imagemfundo, lbNomeMatricula, lbAcesso, lbCadastro, lbInsta;
+	ImageIcon icon2 = new ImageIcon(getClass().getResource("Untitled.png"));
+	JLabel Imagemfundo, lbNomeMatricula, lbAcesso, lbCadastro, lbInsta, lbRodape, lbCabecalho;
 	JLabel lbLogo = new JLabel(icon);
 	JTextField textMatricula;
 	JPasswordField textSenha; //para pegar o texto do password usa getPassword()
@@ -60,13 +64,21 @@ public class InterfaceGUI {
 		
 		//logos
 		panel.add(lbLogo);
-		icon.setImage(icon.getImage().getScaledInstance(250,200,100));
-		lbLogo.setBounds(680,60,240,100);
+		icon.setImage(icon.getImage().getScaledInstance(250,120,100));
+		lbLogo.setBounds(680,60,240,120);
 		ImageIcon iconInsta = new ImageIcon(getClass().getResource("insta.png"));
 		iconInsta.setImage(iconInsta.getImage().getScaledInstance(25,25,100));
 		lbInsta = new JLabel(iconInsta);
 		panel.add(lbInsta);
 		lbInsta.setBounds(700,580,25,25);
+		//rodape
+		lbRodape = new JLabel(); lbCabecalho = new JLabel();
+		lbRodape.setBounds(0,622,1000,40);
+		lbRodape.setBackground(new Color(255,20,147));
+		lbRodape.setOpaque(true);
+		lbCabecalho.setBounds(0,0,1000,30);
+		lbCabecalho.setBackground(new Color(255,20,147));
+		lbCabecalho.setOpaque(true);
 		
 		//clicar no icone do insta ira abrir a pagina do insta
 		lbInsta.addMouseListener(new MouseAdapter() {
@@ -144,6 +156,7 @@ public class InterfaceGUI {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//code do botao entrar
+				acessoProfessor();
 			}
 		});
 		
@@ -154,7 +167,7 @@ public class InterfaceGUI {
 		lbAcesso.setFont(new Font("Verdana", 0, 14));
 		lbCadastro = new JLabel("Cadastre-se");
 		panel.add(lbCadastro);
-		lbCadastro.setBounds(825,500,100,30);
+		lbCadastro.setBounds(825,505,95,20);
 		lbCadastro.setFont(new Font("Verdana", 1, 14));
 		lbCadastro.setForeground(Color.BLUE);
 		novoCadastro(lbCadastro); //chama a funcao novoCadastro
@@ -165,7 +178,7 @@ public class InterfaceGUI {
 	JTextField textNome, textNewMatricula, textNewSenha;
 	
 	//funcao para cadastrar um professor
-	public void novoCadastro(JLabel label) {
+	private void novoCadastro(JLabel label) {
 		label.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -183,7 +196,7 @@ public class InterfaceGUI {
 				lbTextoCadastro.setOpaque(true);
 				lbTextoCadastro.setBounds(100,100,350,30);
 				lbTextoCadastro.setFont(new Font("Verdana",1,22));
-				panelCadastro.add(lbLogo);
+				panelCadastro.add(lbLogo); panelCadastro.add(lbRodape); panelCadastro.add(lbCabecalho);
 				
 				lbBorda = new JLabel();
 				panelCadastro.add(lbBorda);
@@ -260,8 +273,92 @@ public class InterfaceGUI {
 		});
 	}
 	
+	JLabel lbNomeProfessor, lbCampo1, lbCampo2, lbMatriculaProfessor, lbLogOut, 
+	lbLogo2, lbReserva, lbLab1, lbLab2, lbDia;
+
+	private void acessoProfessor() {
+		//configurações do panel
+		panelAcesso = new JPanel();
+		panel.setVisible(false);
+		tela.add(panelAcesso);
+		panelAcesso.setSize(1000,700);
+		panelAcesso.setLayout(null);
+		panelAcesso.setBackground(Color.WHITE);
+
+		//adicionando elementos graficos
+		panelAcesso.add(lbRodape); panelAcesso.add(lbCabecalho);
+		lbLogOut = new JLabel("LogOut");
+		lbLogOut.setFont(new Font("Verdana",0,12));
+		lbLogOut.setBounds(900,40,50,30);
+		panelAcesso.add(lbLogOut);
+		
+		lbLogo2 = new JLabel(icon2);
+		icon2.setImage(icon2.getImage().getScaledInstance(100,50,100));
+		panelAcesso.add(lbLogo2);
+		lbLogo2.setBounds(790,30,100,50);
+		
+		lbReserva = new JLabel("RESERVA DE LABORATÓRIO");
+		panelAcesso.add(lbReserva);
+		lbReserva.setFont(new Font("Verdana",1,16));
+		lbReserva.setBounds(50,50,260,30);
+		
+		//passar o mouse no nome logout
+		lbLogOut.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) { //em cima do nome
+				lbLogOut.setFont(new Font("Verdana",1,12));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { //fora do nome
+				lbLogOut.setFont(new Font("Verdana",0,12));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelAcesso.setVisible(false);
+				tela.add(panel);
+				panel.add(lbLogo);
+				panel.setVisible(true);
+			}
+		});
+		
+		//informacoes do professor
+		lbNomeProfessor = new JLabel("Andrey Roberto Serafim Ferreira");
+		lbMatriculaProfessor = new JLabel("11033358");
+		panelAcesso.add(lbNomeProfessor); panelAcesso.add(lbMatriculaProfessor);
+		lbNomeProfessor.setBounds(50,100,300,30);
+		lbNomeProfessor.setFont(new Font("Verdana",0,14));
+		lbMatriculaProfessor.setBounds(50,130,300,30);
+		lbMatriculaProfessor.setFont(new Font("Verdana",0,12));
+		
+		//esboco da area onde vai ficar o calendario
+		lbCampo1 = new JLabel("Calendario"); lbCampo2 = new JLabel();
+		panelAcesso.add(lbCampo1); panelAcesso.add(lbCampo2);
+		lbCampo1.setBorder(new MatteBorder(1, 1, 1, 1, new Color(169,169,169)));
+		lbCampo1.setBounds(50,170,880,240);
+		lbCampo1.setOpaque(true); lbCampo1.setBackground(new Color(248,248,255));
+		lbCampo2.setBorder(new MatteBorder(1, 1, 1, 1, new Color(169,169,169)));
+		lbCampo2.setBounds(50,440,880,110);
+		//lbCampo2.setOpaque(true); lbCampo2.setBackground(new Color(248,248,255));
+		
+		//info dos labs
+		lbLab1 = new JLabel("Laboratório 1"); lbLab2 = new JLabel("Laboratório 2");
+		panelAcesso.add(lbLab1); panelAcesso.add(lbLab2);
+		lbLab1.setFont(new Font("Verdana",0,14)); lbLab2.setFont(new Font("Verdana",0,14));
+		lbLab1.setBounds(60,480,100,30); lbLab2.setBounds(60,510,100,30);
+		//lbLab1.setForeground(new Color(255,20,147)); lbLab2.setForeground(new Color(255,20,147));
+		
+		lbDia = new JLabel(); //exemplo de data
+		panelAcesso.add(lbDia);
+		lbDia.setFont(new Font("Verdana",0,14));
+		lbDia.setBounds(60,440,200,30);
+		Calendar c = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String dia = sdf.format(c.getTime());
+		lbDia.setText(dia);
+	}
+
 	//main
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		InterfaceGUI teste = new InterfaceGUI();
 		teste.inicializa();
 	}
