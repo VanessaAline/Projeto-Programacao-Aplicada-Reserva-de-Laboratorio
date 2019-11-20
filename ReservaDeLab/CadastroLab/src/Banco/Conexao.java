b87572afef8caf24e6ea697aa07cc357d2789305
andreyroberto <48372956+andreyroberto@users.noreply.github.com>
2019-11-20 11:03:32 -0300

    Update Conexao.java

--- ReservaDeLab/CadastroLab/src/Banco/Conexao.java
+++ ReservaDeLab/CadastroLab/src/Banco/Conexao.java
@@ -15,7 +15,68 @@ import Modelo.Professor;
 public class Conexao {
 	private Connection conexao;
 	
+	/*package Banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexao {
+	private Connection conexao;
+	
 	/*
+	 * Conecta ou cria um bd
+	 */
+	public boolean conectar() {
+		
+		try {
+			String url = "jdbc:sqlite:db/ReservaLab.db";
+			
+			conexao = DriverManager.getConnection(url);
+		}catch(SQLException e) {
+			System.err.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean desconectar() {
+		
+		try {
+			if(conexao.isClosed() == false) {
+				conexao.close();
+			}
+		}catch(SQLException e) {
+			System.err.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+	
+	//cria statement para executar um sql
+	public Statement criarStatement() {
+		try {
+			return conexao.createStatement();
+		}catch(SQLException e) {
+			return null;
+		}
+	}
+	
+	//cria statement para executar um sql
+	public PreparedStatement criarPreparedStatement(String sql) {
+		try {
+			return conexao.prepareStatement(sql);
+		}catch(SQLException e) {
+			return null;
+		}
+	}
+	
+	public Connection getConexao() {
+		return conexao;
+	}
+}
 	 * Conecta ou cria um bd
 	 */
 	public boolean conectar() {
@@ -92,10 +153,10 @@ public class Conexao {
 			if(resultado == 1) {
 				System.out.println("Professor Cadastrado");
 			}else {
-				System.out.println("Professor n„o inserido");
+				System.out.println("Professor n√£o inserido");
 			}
 		}catch(SQLException e) {
-			System.out.println("Professor n„o inserido");
+			System.out.println("Professor n√£o inserido");
 		}finally {
 			if(prepared != null) {
 				try {
