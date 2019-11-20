d9a01bd3fd5c3579fa4b3000d377dc94cc18e070
andreyroberto <48372956+andreyroberto@users.noreply.github.com>
2019-11-20 13:04:47 -0300

    Add files via upload

---
+++ ReservaDeLab/CadastroLab/src/Modelo/Laboratorio.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+public class Laboratorio {
+	
+	private String codigo;
+	private int quantidadeMaquinas;
+	private char bloco;
+	
+	//construtor
+	public Laboratorio(String codigo, int quantidadeMaquinas, char bloco){
+		this.codigo = codigo;
+		this.quantidadeMaquinas = quantidadeMaquinas;
+		this.bloco = bloco;
+	}
+	
+	public Laboratorio(){}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getQuantidadeMaquinas() {
+		return quantidadeMaquinas;
+	}
+
+	public void setQuantidadeMaquinas(int quantidadeMaquinas) {
+		this.quantidadeMaquinas = quantidadeMaquinas;
+	}
+
+	public char getBloco() {
+		return bloco;
+	}
+
+	public void setBloco(char bloco) {
+		this.bloco = bloco;
+	}
+}
