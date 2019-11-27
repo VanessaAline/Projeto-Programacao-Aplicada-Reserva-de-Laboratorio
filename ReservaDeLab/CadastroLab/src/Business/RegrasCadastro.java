f69c6079ed55235004379be0c3269036af8be491
andreyroberto <48372956+andreyroberto@users.noreply.github.com>
2019-11-26 22:15:55 -0300

    Add files via upload

--- ReservaDeLab/CadastroLab/src/Business/RegrasCadastro.java
+++ ReservaDeLab/CadastroLab/src/Business/RegrasCadastro.java
@@ -1,23 +1,13 @@
-package Business;
-
-import Banco.Pesquisar;
-import Modelo.Professor;
-
-public class RegrasCadastro {
-	
-	private Pesquisar consulta = new Pesquisar();
-	
-	public boolean validarCadastro(Professor prof) {
-		if(prof.getMatricula() != null && prof.getNome() != null && prof.getSenha() != null) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean validarAcesso(String matricula, String senha){
-		if(consulta.pesquisarAcesso(matricula, senha)){
-			return true;
-		}
-		return false;
-	}
-}
+//package Business;
+//
+//import Modelo.Professor;
+//
+//public class RegrasCadastro {
+//	
+//	public boolean validarCadastro(Professor prof) {
+//		if(prof.getMatricula() != null && prof.getNome() != null && prof.getSenha() != null) {
+//			return true;
+//		}
+//		return false;
+//	}
+//}
